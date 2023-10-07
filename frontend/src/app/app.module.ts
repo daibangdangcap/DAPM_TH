84a106563c312cedb24c3117b5fb0e53845476d3
daibangdangcap <nguyentienthanh1122003@gmail.com>
2023-10-07 17:49:01 +0700

    xóa sign up component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HeaderComponent } from './client/header/header.component';
 import { FooterComponent } from './client/footer/footer.component';
 import { HomepageComponent } from './client/homepage/homepage.component';
 import { ProductComponent } from './client/product/product.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
 import { RegisterComponent } from './register/register.component';
 import { CartComponent } from './cart/cart.component';
 import { UserPageComponent } from './user-page/user-page.component';
@@ -19,7 +18,6 @@ import { LoginComponent } from './login/login.component';
     FooterComponent,
     HomepageComponent,
     ProductComponent,
-    SignUpComponent,
     RegisterComponent,
     CartComponent,
     UserPageComponent,
