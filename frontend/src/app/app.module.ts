bc28840db0693b65b8db37f02ef3950d9cbe2885
HUYPHOENIX <truongquanghuy2003@gmail.com>
2023-10-07 17:45:08 +0700

    Tao component cho login , register , cart , user-page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { HeaderComponent } from './client/header/header.component';
 import { FooterComponent } from './client/footer/footer.component';
 import { HomepageComponent } from './client/homepage/homepage.component';
 import { ProductComponent } from './client/product/product.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { RegisterComponent } from './register/register.component';
+import { CartComponent } from './cart/cart.component';
+import { UserPageComponent } from './user-page/user-page.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +18,12 @@ import { ProductComponent } from './client/product/product.component';
     HeaderComponent,
     FooterComponent,
     HomepageComponent,
-    ProductComponent
+    ProductComponent,
+    SignUpComponent,
+    RegisterComponent,
+    CartComponent,
+    UserPageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
