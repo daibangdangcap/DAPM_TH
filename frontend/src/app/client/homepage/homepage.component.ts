08d922e9d05b7fe8717e5971fb92c3d8809db5c4
daibangdangcap <nguyentienthanh1122003@gmail.com>
2023-10-14 01:06:54 +0700

    xong phần slider của trang chủ
    xong phần slider của trang chủ
    thay đổi trong file app component.html
    thêm ng bootstrap vào project frontend
    thêm các hình ảnh trong thư mục images được tạo trong thư mục asset

--- frontend/src/app/client/homepage/homepage.component.ts
+++ frontend/src/app/client/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-homepage',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent {
-
+  showNavigationArrows = true;
+	showNavigationIndicators = true;
+	images1 = ['../../../assets/images/bannerA1.png','../../../assets/images/OIP (1).jfif','../../../assets/images/OIP (2).jfif'];
+  images2=['../../../assets/images/bannerB1.png']
+	constructor(config: NgbCarouselConfig) {
+		// customize default values of carousels used by this component tree
+		config.showNavigationArrows = true;
+		config.showNavigationIndicators = true;
+	}
 }
