f15411d28def52756cf6f7557ef75b3e969493d8
daibangdangcap <nguyentienthanh1122003@gmail.com>
2023-10-14 13:17:35 +0700

    chỉnh sửa slider trang chủ và thêm banner
    chỉnh sửa lỗi kích thước trong slider carousel
    thêm ảnh trong thư mục image
    thêm font chữ Baloo Tamma trong folder fonts được tạo trong thư mục asset
    hoàn thành nốt giao diện trang chủ

--- frontend/src/app/client/homepage/homepage.component.ts
+++ frontend/src/app/client/homepage/homepage.component.ts
@@ -7,13 +7,6 @@ import { NgbCarouselConfig, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent {
-  showNavigationArrows = true;
-	showNavigationIndicators = true;
-	images1 = ['../../../assets/images/bannerA1.png','../../../assets/images/OIP (1).jfif','../../../assets/images/OIP (2).jfif'];
-  images2=['../../../assets/images/bannerB1.png']
-	constructor(config: NgbCarouselConfig) {
-		// customize default values of carousels used by this component tree
-		config.showNavigationArrows = true;
-		config.showNavigationIndicators = true;
-	}
+	images1 = ['../../../assets/images/bannerA1.png','../../../assets/images/bannerB1.png','../../../assets/images/OIP (2).jfif'];
+  images2=['../../../assets/images/bannerB1.png','../../../assets/images/bannerA1.png']
 }
