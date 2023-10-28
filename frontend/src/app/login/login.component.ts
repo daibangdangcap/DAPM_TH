ae6e2bdc0c893bb706634c0e71ae25d4b0f3f832
HUYPHOENIX <truongquanghuy2003@gmail.com>
2023-10-28 16:24:19 +0700

    Sửa đổi trang login và register

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component ,ViewChild,ElementRef} from '@angular/core';
+import { Component ,ViewChild} from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
