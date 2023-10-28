8a097d0f818a451cb9b3464f5c2027c6426b3123
dziencute <lethanhduyan@gmail.com>
2023-10-28 17:16:01 +0700

    Fix đường dẫn
    - Sắp xếp thư mục trong Client

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './client/login/login.component';
 import { HomepageComponent } from './client/homepage/homepage.component';
 
 const routes: Routes = [
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,12 @@ import { HeaderComponent } from './client/header/header.component';
 import { FooterComponent } from './client/footer/footer.component';
 import { HomepageComponent } from './client/homepage/homepage.component';
 import { ProductComponent } from './client/product/product.component';
-import { RegisterComponent } from './register/register.component';
-import { CartComponent } from './cart/cart.component';
-import { UserPageComponent } from './user-page/user-page.component';
-import { LoginComponent } from './login/login.component';
-import { CatalogComponent } from './catalog/catalog.component';
-import { CheckoutComponent } from './checkout/checkout.component';
+import { RegisterComponent } from './client/register/register.component';
+import { CartComponent } from './client/cart/cart.component';
+import { UserPageComponent } from './client/user-page/user-page.component';
+import { LoginComponent } from './client/login/login.component';
+import { CatalogComponent } from './client/catalog/catalog.component';
+import { CheckoutComponent } from './client/checkout/checkout.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
