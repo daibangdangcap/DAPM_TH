20b5959ba8490fc21382695e331b1cdb9af0c62b
peongox <bngoc.hi4103@gmail.com>
2023-11-04 22:26:27 +0700

    DT-47: Cập nhật trang Catalog
    - thêm path cho trang Catalog trong app-routing.
    - bổ sung css.
    - hiển thị hình ảnh theo thẻ a tương ứng.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './client/login/login.component';
 import { HomepageComponent } from './client/homepage/homepage.component';
 import { ProductComponent } from './client/product/product.component';
+import { CatalogComponent } from './client/catalog/catalog.component';
 
 const routes: Routes = [
   // CLIENT
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'HomePage', component: HomepageComponent },
   { path :'user/account', component: LoginComponent},
   { path: 'product',component:ProductComponent},
+  {path: 'Catalog', component: CatalogComponent}
 ];
 
 @NgModule({
--- frontend/src/app/client/catalog/catalog.component.ts
+++ frontend/src/app/client/catalog/catalog.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.css']
 })
-export class CatalogComponent {
+export class CatalogComponent implements OnInit {
+  activeIndex = 0;  
+  selectedImage: string = 'assets/images/client/homepage/andoduong.png';
+  imageNumber: number = 1;
+  // images: string[] = ['assets/images/client/homepage/andoduong.png', 'assets/images/client/homepage/thaibinhduong.png', 'assets/images/client/homepage/bacbangduong.png', 'assets/images/client/homepage/namdaiduong.png'];
+  constructor() {}
+  ngOnInit(): void {
+  }
+  onClickItem(index: number) {
+    this.activeIndex = index;
+  }
 
+  showImage(imageNumber: number) {
+    switch (imageNumber) {
+      case 1:
+        this.selectedImage  = 'assets/images/client/homepage/andoduong.png';
+        break;
+      case 2:
+        this.selectedImage = 'assets/images/client/homepage/thaibinhduong.png';
+        break;
+      case 3:
+        this.selectedImage = 'assets/images/client/homepage/bacbangduong.png';
+        break;
+      case 4:
+        this.selectedImage = 'assets/images/client/homepage/namdaiduong.png';
+        break;
+    }
+  }
 }
