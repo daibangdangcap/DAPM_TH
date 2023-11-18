dd9379be7833e29596f1ab8bdcbfccb4e36f2cdf
HUYPHOENIX <truongquanghuy2003@gmail.com>
2023-11-18 14:43:00 +0700

    Cập nhật giao diện cho tìm kiếm + thêm background cho các sản phẩm

--- frontend/src/app/client/searchpage/searchpage.component.ts
+++ frontend/src/app/client/searchpage/searchpage.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SearchService } from 'src/app/services/search.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-searchpage',
   templateUrl: './searchpage.component.html',
@@ -10,7 +10,7 @@ import { SearchService } from 'src/app/services/search.service';
 export class SearchpageComponent implements OnInit {
   searchResults: any[];
 
-  constructor(private route: ActivatedRoute, private proService : SearchService) {}
+  constructor(private route: ActivatedRoute, private proService : SearchService , private router:Router) {}
 
   ngOnInit() {
     this.route.queryParams.subscribe((params) => {
@@ -19,6 +19,7 @@ export class SearchpageComponent implements OnInit {
         this.proService.searchProducts(tenSP).subscribe(
           (data) => {
             this.searchResults=data.data;
+            console.log(this.searchResults)
           },
           (error) => {
             console.error(error);
@@ -26,5 +27,8 @@ export class SearchpageComponent implements OnInit {
         );
       }
     });
+}
+changeToDetailPage(idSanPham){
+  this.router.navigate([`/Product-Detail`,idSanPham])
 }
 }
